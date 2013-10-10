f99e9f9fca7b6a662c9f2c5253914b322ecb2ce3
AlexXXXX1988 <Alex@85-54.priv23.nus.edu.sg>
2013-10-10 13:40:12 +0800

    canopykmeans

--- canopykmeans/server.R
+++ canopykmeans/server.R
@@ -35,7 +35,7 @@ shinyServer(function(input, output) {
     rslt$t1t2<-c(t1,t2)
     rslt
   })
-  output$canopies<-renderPlot({
+  output$canopies<-renderImage({
     df<-cnprslt()[[1]]
     crclt1<-lapply(cnprslt()[[2]],function(x)circle(x,cnprslt()$t1t2[1]))
     crclt2<-lapply(cnprslt()[[2]],function(x)circle(x,cnprslt()$t1t2[2]))
@@ -44,15 +44,30 @@ shinyServer(function(input, output) {
       p<-p+geom_path(data=crclt1[[i]],aes(x,y,col=factor("t1")))
       p<-p+geom_path(data=crclt2[[i]],aes(x,y,col=factor("t2")))
     }
-    print(p) 
-  }
-    )
+    cnpypic<-tempfile(fileext='.png')
+    png(cnpypic, width=800, height=600)
+    print(p)
+    dev.off()
+    list(src = cnpypic,
+         contentType = 'image/png',
+         width = 800,
+         height = 600,
+         alt = "This is alternate text")
+  }, deleteFile = TRUE)
   output$t1t2<-renderPrint(data.frame(T1=cnprslt()$t1t2[1],T2=cnprslt()$t1t2[2]))
-  output$kmeans<-renderPlot({
+  output$kmeans<-renderImage({
       df<-cnprslt()[[1]]
       p<-ggplot(df,aes(x,y,col=factor(cluster)))+geom_point()
+      outfile <- tempfile(fileext='.png')
+      png(outfile, width=800, height=600)
       print(p)
-      })
+      dev.off()
+      list(src = outfile,
+           contentType = 'image/png',
+           width = 800,
+           height = 600,
+           alt = "This is alternate text")
+  }, deleteFile = TRUE)
   output$summary<-renderPrint(listtodf(cnprslt()[[2]]))
   output$cnt<-renderTable(cnprslt()[[1]])
 }
